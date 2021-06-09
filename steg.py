dba0dadf7ad6558b174da6ede30b74fa47ebed98
Muhammad Ashir <mashir670@gmail.com>
2021-06-09 22:10:21 +0500

    using argparse now

--- steg.py
+++ steg.py
@@ -1,12 +1,24 @@
 
 # third-party Libs
+from genericpath import exists
 import PIL as pil
 from PIL import Image
 
 # built-in Libs
 import os
 from datetime import datetime
+import argparse
 import sys
+
+# command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--action", "-a", help="which action to perform. actions are either 'encode' or 'decode'")
+parser.add_argument("--image", "-i", help="address of image to save text of extract text from (depending on action)")
+parser.add_argument("--payload", "-p", help="payload to save")
+parser.add_argument("--destination", "-d", help="destination address for saving the manipulated image")
+parser.add_argument("--tofile", "-f", help="address of text file to which, the extracted text will be written. Only pass it when action=decode, else the program will exit")
+parser.add_argument("--fromfile", action="store_true" ,help="if passed, the payload will be used as address of text file from which text will be read and saved to an image. Only pass when action=encode.")
+args = parser.parse_args()
 
 # function for binary conversion (8-bit)
 def binary(num):
@@ -54,6 +66,7 @@ def hide(image, payload, dest):
             left_over = Payload(payload[(width * height):])
         else:
             payload = Payload(payload)
+
         for x in range(0, width):
             for y in range(0, height):
                 pixel = list(img.getpixel((x, y)))
@@ -63,6 +76,7 @@ def hide(image, payload, dest):
                         i += 1
                 img.putpixel((x, y), tuple(pixel))
         img.save(str(destination), "PNG")
+
         return left_over
 
 
@@ -87,9 +101,11 @@ def show(image):
             for n in range(0, len(pixel)):
                 result = result + str(pixel[n] & 1)
         pixel.clear()
+
         length = decimal16(result)
         print(length)
         result = ""
+
         for x in range(0, width):
             for y in range(0, height):
                 pixel = list(img.getpixel((x, y)))
@@ -110,6 +126,7 @@ def decode(msg):
         array.append(msg[a:i])
         a = i
     array.append(msg[a:])
+
     a = 0
     for i in array:
         i = i[::-1]
@@ -118,28 +135,76 @@ def decode(msg):
                 a += 2 ** e
         result = result + str(chr(a))
         a = 0
+
     return result
 
-# main function
-def main():
-    
+# main function 
+# args = command line arguments
+def main(args):
     try:
-        if sys.argv[1] == ("hide"):
-            image = str(sys.argv[2])
-            payload = str(sys.argv[3])
-            with open(payload, "r") as file:
-                payload = file.read()
-            save_loc = str(sys.argv[4])
-            left_over = hide(image, payload, save_loc)
-            
+        # taking care of command line arguments and declaring needed variables
+        if args.action is not None:
+            if args.action == 'encode' or args.action == "decode":
+                action = args.action
+            else:
+                sys.exit("{} is not a valid action. Run with -h for more info.")
         else:
-            image = str(sys.argv[2])
-            result = show(image)
-            print(result)
-        print("\ndone")
+            sys.exit("No action defined, use --action or -a to define the action. For more info run with -h.")
+
+        if action == "encode":
+            # if action==encode, make sure image, payload, dest arguments are provided.
+            if args.image is not None:
+                img = args.image
+                assert os.path.exists(img), "{} does not exist.".format(img)
+                assert img.endwith(".png"), "Only png format is support."
+            else:
+                sys.exit("image is not defined. Define one with -i or --image flag. Run with -h for more info.")
+            if args.payload is not None:
+                if args.fromfile:
+                    assert (args.payload).endswith(".txt"), "make sure you provide the location of a text file."
+                    assert os.path.exists(args.payload), "{} does not exist.".format(args.payload)
+                    with open(args.payload, 'r') as f:
+                        payload = f.read()
+                        f.close()
+                else:
+                    payload = args.payload
+            else:
+                sys.exit("No payload is passed. Define the payload with -p or --payload flag. Run with -h for more info.")
+            if args.destination is not None:
+                assert args.destination.endswith(".png"), "image can be only saved as png"
+                dest = args.destination
+            else:
+                sys.exit("No address to save the image is passed, pass it with -d or --destination flag. Run with -h for more info.")
+            
+            # encoding payload in img
+            hide(img, payload, dest)
+
+        else: 
+            # if action==decode, make sure the image is provided
+            if args.image is not None:
+                assert (args.image).endswith(".png"), "Only png is supported."
+                assert os.path.exists(args.image), "{} does not exist.".format(args.image)
+                img = args.image
+            else:
+                sys.exit("No image defined to extract text from. Pass with -i or --image. Run with -h for help.")
+            
+            # extracting text for image(img)
+            result = show(img)
+
+            # write text to file if 
+            if args.tofile is not None:
+                assert (args.tofile).endswith(".txt"), "file to save text should have extension '.txt'"
+                with open(args.tofile, 'w') as f:
+                    f.write(result)
+                    print("text saved to {}".format(args.tofile))
+                    f.close()
+            else:
+                print(result)
+
     except Exception as e:
         print(e)
-
+    finally:
+        print("done")
 
 if __name__ == '__main__':
-    main()
+    main(args)
